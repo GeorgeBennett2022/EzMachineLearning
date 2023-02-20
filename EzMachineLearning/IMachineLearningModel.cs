1b30493eb94d7b891ca18850769d6fe501f57d68
GeorgeBennett <georgebennett3478@gmail.com>
2023-02-19 19:34:22 -0500

    Add project files.

---
+++ EzMachineLearning/IMachineLearningModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzMachineLearning.Interfaces
+{
+    internal interface IMachineLearningModel
+    {
+        public void Fit(double[][] x, double y);
+
+        public double[] Predict(double[][] x);
+    }
+}
---
+++ EzMachineLearning/LossFunctions/LossFunctions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EzMachineLearning.Exceptions;
+
+namespace EzMachineLearning.LossFunctions
+{
+    public static class LossFunctions
+    {
+        public static Dictionary<LossFunc, Func<double[], double>> LossFuncs { get; }
+            = new Dictionary<LossFunc, Func<double[], double>>
+            {
+                { LossFunc.MeanSquaredError, MeanSquaredError },
+                { LossFunc.MSE, MeanSquaredError },
+                { LossFunc.RootMeanSquaredError, RootMeanSquaredError },
+                { LossFunc.RMSE, RootMeanSquaredError },
+                { LossFunc.MeanAbsoluteError, MeanAbsoluteError },
+                { LossFunc.MAE, MeanAbsoluteError }
+            };
+
+        public static double MeanSquaredError(double[] error)
+        {
+            return -1 * error.Select(a => Math.Pow(a, 2)).Average();
+        }
+
+        public static double RootMeanSquaredError(double[] error)
+        {
+            return -1 * Math.Sqrt(MeanSquaredError(error));
+        }
+
+        public static double MeanAbsoluteError(double[] error)
+        {
+            return -1 * error.Select(a => Math.Abs(a)).Average();
+        }
+    }
+
+    public enum LossFunc
+    {
+        MeanSquaredError,
+        MSE,
+        RootMeanSquaredError,
+        RMSE,
+        MeanAbsoluteError,
+        MAE
+    }
+}
